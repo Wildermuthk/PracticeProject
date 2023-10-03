75ffa4fddfe0fd052f6ec836ca4916352bd2a61a
Wildermuthk <Wildermuthk@cwu.edu>
2023-10-02 21:36:17 -0700

    Added the file Practice.java.

---
+++ Practice.java
@@ -0,0 +1,12 @@
+//Katelyn Wildermuth
+//CS380
+
+public class Practice{
+
+    public static void main(String[] args){
+        System.out.print("This is to practice using commands in Git.");
+    }
+    
+
+
+}
