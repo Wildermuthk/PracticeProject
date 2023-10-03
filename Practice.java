f4fa17279059db4416e20e1028c56aef6e1e7b20
Wildermuthk <Wildermuthk@cwu.edu>
2023-10-02 22:44:25 -0700

    Updated Dog class.

--- Practice.java
+++ Practice.java
@@ -11,10 +11,12 @@ public class Practice{
 
 public class Dog{
     private String name;
+    private String breed;
     private int age;
 
-    public Dog(String name, int age){
+    public Dog(String name, String breed, int age){
         this.name = name;
+        this.breed = breed;
         this.age = age;
     }
 }
